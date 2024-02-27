4f77485c94a9fe609d9dcd39ccdc0945a6b72ec2
Brice Grenard <brice.grenard@gmail.com>
2024-02-27 19:46:46 +0100

    UserId must be a string.

--- KitchenOrderDto.cs
+++ KitchenOrderDto.cs
@@ -5,7 +5,7 @@ namespace PizzaMauiApp.Kitchen;
 public class KitchenOrderDto
 {
     public Guid OrderId { get; set; }
-    public Guid UserId { get; set; }
+    public string UserId { get; set; } = null!;
     public List<IOrderItem>? Items { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime ExpiresAt { get; set; }
